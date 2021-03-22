73c5c451967d4496f74c1fe8ee0fbb445ea7a511
Daw588 <dave05.irl@gmail.com>
2021-03-22 12:52:40 -0500

    0.0.1 Update
    Alpha release of the game.

---
+++ src/settings.237af.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"cd2f6",main:"c5e8b"}};
